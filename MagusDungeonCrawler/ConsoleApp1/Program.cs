939bc4048a3fc9658cc6fdc3f1659a5f0d65df97
suflors <iammagical.space@gmail.com>
2023-12-15 13:40:58 -0800

    added the main loop of the dungeon crawler

--- MagusDungeonCrawler/ConsoleApp1/Program.cs
+++ MagusDungeonCrawler/ConsoleApp1/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Apple, World!");
+var keepGoing = true;
+while (keepGoing)
+{
+	Console.WriteLine();
+	Console.WriteLine("Where would you like to go?");
+	Console.WriteLine("(N for North, S for South, E for East, or W for West.)");
+	var pressedKey = Console.ReadKey(false);
+}
