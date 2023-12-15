142c7cace03673cb074f635938ea91a21339abaa
suflors <iammagical.space@gmail.com>
2023-12-15 14:04:34 -0800

    added a multidimensional array to represent the dungeon

--- MagusDungeonCrawler/ConsoleApp1/Program.cs
+++ MagusDungeonCrawler/ConsoleApp1/Program.cs
@@ -1,7 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
 Console.WriteLine("-= Welcome to the Dungeon! =-");
 
 var keepGoing = true;
+var position = (South: 5, East: 5);
+
+const string nsHallway = "You have entered a hallway with exits to the North and South.";
+const string ewHallway = "You have entered a hallway with exits to the East and West.";
+const string esCorner = "You have entered a corner with exits to the East and South.";
+const string neCorner = "You have entered a corner with exits to the North and East.";
+const string nwCorner = "You have entered a corner with exits to the North and West.";
+
+// Dungeon structure:
+// ============
+// =    X     =
+// =   XX  X  =
+// =   X   X  =
+// =   X   X  =
+// =   XXXXXX =
+// =     O    = <-- You start at the O here, position 5, 5 from the top left
+// =     X    =
+// ============
+var rooms = new[]
+{
+	new [] { null, null, null, null, "You can go no further North! The only exit is south.", null, null, null, null, null,},
+	new [] { null, null, null, esCorner, nwCorner, null, null, "A dead end! The only exit is South.", null, null },
+	new [] { null, null, null, nsHallway, null, null, null, nsHallway, null, null},
+	new [] { null, null, null, nsHallway, null, null, null, nsHallway, null, null},
+	new [] { null, null, null, neCorner, ewHallway, "You have reached a crossroad. Exits are South, East, and West.", ewHallway, "You have reached a crossroad. Exits are North, East, and West.", "A dead end! The only exit is West.", null },
+	new [] { null, null, null, null, null, nsHallway, null, null, null, null},
+	new [] { null, null, null, null, null, "You can go no further South! The only exit is North.", null, null, null, null }
+};
 
 while (keepGoing)
 {
